901db9fc9f38f9d4ead239b91c0ca38e49aa038e
darektoa <darektoa@gmail.com>
2020-12-01 16:15:52 +0700

    Using Workbox

--- service-worker.js
+++ service-worker.js
@@ -1,77 +1,51 @@
-const CACHE_NAME = 'infobola-v1.0';
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/detail.html",
-  "/manifest.json",
-  "/css/style.css",
-  "/css/materialize.min.css",
-  "/images/icon-144.png",
-  "/images/icon-192.png",
-  "/images/icon-512.png",
-  "/pages/premier-league.html",
-  "/pages/championship.html",
-  "/pages/saved.html",
-  "/js/notification-request.js",
-  "/js/sw-register.js",
-  "/js/materialize.min.js",
-  "/js/api.js",
-  "/js/nav.js",
-  "/js/db.js",
-  "/js/idb.js",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-];
- 
-self.addEventListener("install", function(event) {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+workbox.precaching.precacheAndRoute([
+    {url: '/', revision: '1'},
+    {url: '/nav.html', revision: '1'},
+    {url: '/index.html', revision: '1'},
+    {url: '/detail.html', revision: '1'},
+    {url: '/manifest.json', revision: '1'},
+    {url: '/css/style.css', revision: '1'},
+    {url: '/css/materialize.min.css', revision: '1'},
+    {url: '/images/icon-144.png', revision: '1'},
+    {url: '/images/icon-192.png', revision: '1'},
+    {url: '/images/icon-512.png', revision: '1'},
+    {url: '/pages/premier-league.html', revision: '1'},
+    {url: '/pages/championship.html', revision: '1'},
+    {url: '/pages/saved.html', revision: '1'},
+    {url: '/js/notification-request.js', revision: '1'},
+    {url: '/js/sw-register.js', revision: '1'},
+    {url: '/js/materialize.min.js', revision: '1'},
+    {url: '/js/api.js', revision: '1'},
+    {url: '/js/nav.js', revision: '1'},
+    {url: '/js/db.js', revision: '1'},
+    {url: '/js/idb.js', revision: '1'},
+    {url: 'https://fonts.googleapis.com/icon?family=Material+Icons', revision: '1'},
+    {url: 'https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2', revision: '1'},
+], {
+  ignoreUrlParametersMatching: [/.*/],
+});
+
+
+
+workbox.routing.registerRoute(
+    new RegExp('/pages'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
     })
-  );
-});
+);
 
-self.addEventListener("fetch", function(event) {
-  const base_url = "https://api.football-data.org/";
-  const {onLine} = navigator;
 
-  if (event.request.url.includes(base_url) && onLine) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then((cache) => {
-        return fetch(event.request).then((response) => {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
-    );
-  } else {
-    event.respondWith(
-      caches
-      .match(event.request, { ignoreSearch: true })
-      .then((response) => {
-          return response || fetch (event.request);
-      })
-    )
-  }
-});
 
-self.addEventListener("activate", function(event) {
-  clients.claim();
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
+workbox.routing.registerRoute(
+    new RegExp('(https://|http://)'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'data-api'
     })
-  );
-});
+);
+
+
 
 self.addEventListener('push', function(event) {
     const body = event.data ? event.data.text() : 'Push message no payload';
